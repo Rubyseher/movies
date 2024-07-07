918f8d6a9c8281e09fc5613397d9501d567a1ca7
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2024-07-07 15:45:13 +0530

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Image from "next/image";
+import "@radix-ui/themes/styles.css";
+import { Button,Theme } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <main>
-      <Image src="/Home.jpg"  layout='fill' alt="kitty" />
+      <Theme>
+        <Image src="/Home.jpg" layout="fill" alt="kitty" className="-z-50 fixed" />
+        <div className="flex flex-col items-center  h-screen">
+          <Button color="crimson" variant="soft" radius="large" size="3" style={{marginTop:'14rem',width:'6rem',letterSpacing:'1px'}}>
+            <b>LOGIN</b>
+          </Button>
+        </div>
+      </Theme>
     </main>
   );
 }
