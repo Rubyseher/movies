fe76853cd7a664e63e528bbfded8dd47a058bc9d
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2024-07-07 17:55:03 +0530

    button radix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import "@radix-ui/themes/styles.css";
-import { Button,Theme } from "@radix-ui/themes";
+import { Button, Theme } from "@radix-ui/themes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -9,8 +11,15 @@ export default function Home() {
       <Theme>
         <Image src="/Home.jpg" layout="fill" alt="kitty" className="-z-50 fixed" />
         <div className="flex flex-col items-center  h-screen">
-          <Button color="crimson" variant="soft" radius="large" size="3" style={{marginTop:'14rem',width:'6rem',letterSpacing:'1px'}}>
-            <b>LOGIN</b>
+          <Button
+            color="crimson"
+            variant="soft"
+            radius="large"
+            size="3"
+            style={{ marginTop: "14rem", letterSpacing: "1px", display: "inline-block", width: "14rem", fontWeight: "800" }}
+          >
+            <FontAwesomeIcon icon={faArrowRightToBracket} style={{ width: "22px", display: "inline-flex" ,marginRight:'10px'}} />
+            LOGIN
           </Button>
         </div>
       </Theme>
